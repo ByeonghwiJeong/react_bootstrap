dcd09d17aa1c7175d540170741c93a85a629e7ef
ByeonghwiJeong <byeonghwijeong@gmail.com>
2023-01-27 03:02:57 +0900

    feat : grid_system & process.env.PUBLIC_RUL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import "./App.css";
-import bg from "./img/bg.png";
 
 function App() {
   return (
@@ -16,9 +15,32 @@ function App() {
         </Container>
       </Navbar>
       <Button variant="outline-primary">Primary</Button>
-      <div
-        className="main-bg"
-        style={{ backgroundImage: "url(" + bg + ")" }}></div>
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + "/logo192.png"} width="80%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
