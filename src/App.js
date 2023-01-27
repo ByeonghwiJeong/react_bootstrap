4a40d833dfdc66b80b877f7ca387c7e7a5200625
ByeonghwiJeong <byeonghwijeong@gmail.com>
2023-01-27 15:57:19 +0900

    feat : useNavigate, Outlet
    - 페이지 이동 : useNavigate
      - let navigate = useNavigate();
      - 버튼에 onclick={()=>{ navigate('/detail')}}
    - 404페이지 처리
      - <Route path="*" element={ <div>없는페이지임</div> } />
    - nested routes
      - before
      <Route path="/about/member" element={ <div>멤버들</div> } />
      <Route path="/about/location" element={ <div>회사위치</div> } />
      - after
      <Route path="/about" element={ <About/> } >
        <Route path="member" element={ <div>멤버들</div> } />
        <Route path="location" element={ <div>회사위치</div> } />
      </Route>
    - div보이지 않음
      - function About() 안에 <Outlet></Outlet>

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { useState } from "react";
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import "./App.css";
 import data from "./data";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import Detail from "./Detail";
 
 function App() {
   let [shoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -14,9 +15,18 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}>
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}>
+              Detail
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -40,6 +50,13 @@ function App() {
             </div>
           }></Route>
         <Route path="/detail" element={<Detail></Detail>}></Route>
+        <Route path="/about" element={<About></About>}>
+          <Route path="member" element={<div>멤버임</div>}></Route>
+          <Route path="location" element={<About></About>}></Route>
+          <Route></Route>
+        </Route>
+
+        <Route path="*" element={<div>없는페이지</div>}></Route>
       </Routes>
     </div>
   );
@@ -56,6 +73,15 @@ function Card(props) {
       />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.price}</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
     </div>
   );
 }
