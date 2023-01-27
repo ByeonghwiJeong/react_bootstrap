1f277b258346548270742127cc44845b3a907479
ByeonghwiJeong <byeonghwijeong@gmail.com>
2023-01-27 14:30:00 +0900

    feat : Card컴포턴트 Map사용

--- src/App.js
+++ src/App.js
@@ -22,9 +22,12 @@ function App() {
       <div className="main-bg"></div>
       <div className="container">
         <div className="row">
-          <Card shoes={shoes[0]} i={1}></Card>
+          {/* <Card shoes={shoes[0]} i={1}></Card>
           <Card shoes={shoes[1]} i={2}></Card>
-          <Card shoes={shoes[2]} i={3}></Card>
+          <Card shoes={shoes[2]} i={3}></Card> */}
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i + 1}></Card>;
+          })}
         </div>
       </div>
     </div>
@@ -35,7 +38,9 @@ function Card(props) {
   return (
     <div className="col-md-4">
       <img
-        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
         width="80%"
       />
       <h4>{props.shoes.title}</h4>
