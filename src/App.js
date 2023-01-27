6f386d175c6e422f014ce2e74823978d5a62218a
ByeonghwiJeong <byeonghwijeong@gmail.com>
2023-01-27 14:02:05 +0900

    feat : export & import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import "./App.css";
+import data from "./data";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -19,25 +23,28 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + "/logo192.png"} width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </div>
           <div className="col-md-4">
             <img
               src="https://codingapple1.github.io/shop/shoes2.jpg"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
           </div>
           <div className="col-md-4">
             <img
               src="https://codingapple1.github.io/shop/shoes3.jpg"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
           </div>
         </div>
       </div>
